d3454f55d1109aab0b6a156b89e2328efab3dde3
Julia Menchavez <jcmenchavez@gmail.com>
2014-12-30 22:14:59 +0800

    modified error printing

--- classifier.py
+++ classifier.py
@@ -227,4 +227,4 @@ plt.show()
 #     else:
 #         ctr += 1
 # for (label, guess, tweet) in sorted(errors):
-#   print('correct label=%-8s guess=%-8s tweet=%-30s' % (label, guess, tweet))
+#   print('label=%-8s guess=%-8s tweet=%-30s' % (label, guess, tweet))
