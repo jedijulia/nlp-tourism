604267c6705e16c5bba205d822b3f58ec122a319
Julia Menchavez <jcmenchavez@gmail.com>
2015-01-04 16:22:44 +0800

    modified clean function

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -4,10 +4,6 @@ import re
 
 def clean(tweet):
     clean = re.sub(r'https?:\/\/\w+(\.\w+)*(:\w+)?(/[A-Za-z0-9-_\.]*)* ?', '', tweet)
-    clean = re.sub(r'#', '', clean)
-    clean = re.sub(r'!', '', clean)
-    clean = re.sub(r'\.\.\.', '', clean)
-    clean = re.sub(r',', '', clean)
     return clean
 
 def process_data(positive_file, negative_file):
