3872419210b7315599c982305469b6d8dfe501a0
Julia Menchavez <jedijulia@users.noreply.github.com>
2015-01-10 06:52:31 +0800

    import

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -2,6 +2,8 @@
 import random
 import re
 
+from nltk.corpus import stopwords
+
 def clean(tweet):
     clean = re.sub(r'https?:\/\/\w+(\.\w+)*(:\w+)?(/[A-Za-z0-9-_\.]*)* ?', '', tweet)
     clean = re.sub(r'#', '', clean)
