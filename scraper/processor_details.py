ccfb11b0c61caa47c675476500f57303f0ffb296
Julia Menchavez <jcmenchavez@gmail.com>
2015-03-06 09:07:26 +0800

    aded processor for tripadvisor items with details, generates dict and mm for lda

---
+++ scraper/processor_details.py
@@ -0,0 +1,38 @@
+import json
+
+from gensim import corpora
+from nltk.corpus import stopwords
+
+def clean(word):
+    word = word.lower()
+    for symbol in symbols:
+        word = word.replace(symbol, '')
+    return word
+
+items = json.loads(open('tourism/tripadvisor_details_items.json').read())
+stopwords = stopwords.words('english')
+texts = []
+symbols = ['&', '-', ' ', '', '...', '---', '(', ')', '?', '!']
+
+for item in items:
+    contents = ''
+    details = ''
+    if len(item['place']) > 1:
+        place = item['place'][1].replace('\n', '')
+    for content in item['content']:
+        contents += ' ' + content.replace('\n', '')
+    for detail in item['details']:
+        details += ' ' + detail.replace('\n', '')
+    combined = place + ' ' + contents + ' ' + details
+    curr = []
+    for word in combined.split():
+        word = clean(word)
+        if word not in stopwords and word not in symbols:
+            curr.append(word)
+    texts.append(curr)
+
+dictionary = corpora.Dictionary(texts)
+dictionary.save('tripadvisor.dict')
+print dictionary
+corpus = [dictionary.doc2bow(text) for text in texts]
+corpora.MmCorpus.serialize('tripadvisor.mm', corpus)
