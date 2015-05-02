3d8c67da17fbaf8a137ebd51af8d12a8672a0ee2
Julia Menchavez <jcmenchavez@gmail.com>
2015-05-03 01:38:06 +0800

    tweets classified and classifications printed

--- application/nlptourism/phtweetmap/apps.py
+++ application/nlptourism/phtweetmap/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.dispatch import receiver
 
+from classifier.classifier import train, feature_extractor_lda_tripadvisor_top_words_weights
 import streamer
 
 class PhtweetmapAppConfig(AppConfig):
@@ -8,7 +9,10 @@ class PhtweetmapAppConfig(AppConfig):
     verbose_name = "Philippine Tourism Tweet Map"
     flag = False
     def ready(self):
+        classif = train()
         @receiver(streamer.tweet_retrieved)
         def my_callback(sender, **kwargs):
-            print kwargs['tweet']
+            tweet = kwargs['tweet']['text'].encode('utf-8')
+            label = classif.classify(feature_extractor_lda_tripadvisor_top_words_weights(tweet))
+            print 'Label: {} || Tweet: {}'.format(label, tweet)
         streamer.stream()
