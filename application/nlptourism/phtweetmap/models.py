d24b55b388f29e643404f8b257178d07d86f1f01
Julia Menchavez <jcmenchavez@gmail.com>
2015-05-03 02:48:51 +0800

    created Tweet model

--- application/nlptourism/phtweetmap/models.py
+++ application/nlptourism/phtweetmap/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Tweet(models.Model):
+	tweet_id = models.IntegerField()
+	user = models.CharField(max_length=240)
+	lat = models.DecimalField(decimal_places=20, max_digits=25)
+	lng = models.DecimalField(decimal_places=20, max_digits=25)
+	text = models.CharField(max_length=240)
+	sentiment = models.CharField(max_length=100)
