1089736c27f99d9ba83f329a7e2913a3de2e5b23
Julia Menchavez <jcmenchavez@gmail.com>
2015-05-03 18:04:29 +0800

    modified Tweet model, added retrieved field

--- application/nlptourism/phtweetmap/models.py
+++ application/nlptourism/phtweetmap/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Tweet(models.Model):
-	tweet_id = models.IntegerField()
-	user = models.CharField(max_length=240)
-	lat = models.DecimalField(decimal_places=20, max_digits=25)
-	lng = models.DecimalField(decimal_places=20, max_digits=25)
-	text = models.CharField(max_length=240)
-	sentiment = models.CharField(max_length=100)
+    tweet_id = models.IntegerField()
+    user = models.CharField(max_length=240)
+    lat = models.DecimalField(decimal_places=20, max_digits=25)
+    lng = models.DecimalField(decimal_places=20, max_digits=25)
+    text = models.CharField(max_length=240)
+    sentiment = models.CharField(max_length=100)
+    retrieved = models.BooleanField(default=False)
